4aded905550e5302b1e4aeefce1a801fb0b65fe3
Cristian Chiovari <cristian@chiovari.net>
2014-04-13 21:29:14 +0200

    url rewrite

---
+++ JeePlayground/ServletCookieSample/src/main/java/com/javarecipes/servletcookiesample/LoginServletWithURLRewritting.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.javarecipes.servletcookiesample;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author cristianchiovari
+ */
+@WebServlet(name = "LoginServletWithURLRewritting", urlPatterns = {"/LoginServletWithURLRewritting"})
+public class LoginServletWithURLRewritting extends HttpServlet {
+
+     private static final long serialVersionUID = 1L;
+    private final String userID = "cris";
+    private final String password = "cris";
+    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+         String user = request.getParameter("user");
+        String pwd = request.getParameter("pwd");
+         
+        if(userID.equals(user) && password.equals(pwd)){
+            HttpSession session = request.getSession();
+            session.setAttribute("user", "cris");
+            //setting session to expiry in 30 mins
+            session.setMaxInactiveInterval(30*60);
+            Cookie userName = new Cookie("user", user);
+            response.addCookie(userName);
+            //Get the encoded URL string
+            String encodedURL = response.encodeRedirectURL("LoginSuccessWithURLRewritting.jsp");
+            response.sendRedirect(encodedURL);
+        }else{
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out= response.getWriter();
+            out.println("<font color=red>Either user name or password is wrong.</font>");
+            rd.include(request, response);
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
